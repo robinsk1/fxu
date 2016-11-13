e70abb7b8837cfbd77f7eaad82434139ad61ebf7
robinsk1 <robinskilling@gmail.com>
2016-11-13 15:25:30 +0100

    rspec, pry, foundation

---
+++ lib/exchange_rate.rb
@@ -0,0 +1,30 @@
+class ExchangeRate
+
+  class << Self
+
+    def at(date, base_currency, counter_currency)
+
+    end
+
+    def parse_xml
+
+    end
+
+    def get_currency_data
+
+    end
+
+
+    def update_rates
+
+    end
+
+    private
+
+    def calculate_rates
+
+    end
+
+  end
+
+end
